3c5b7909ae92f36bc04c0f0f20e4fee857d24998
Joe Grazewski <jgrazew@gmail.com>
2017-03-06 06:43:51 -0500

    Create enumDirection.h

---
+++ enumDirection.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef ENUMDIRECTION_H
+#define ENUMDIRECTION_H
+
+//will be used for direction of snake
+enum e_direction
+{
+	STOP,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+};
+#endif // !ENUMDIRECTION_H
